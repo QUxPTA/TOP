2f9c853cac52a2095df1175506c4bf2146e5d7c6
QUxPTA <125445356+QUxPTA@users.noreply.github.com>
2025-07-21 15:55:34 +0300

    Update implementation to include comments for clarity

--- ruby/caesar_cipher.rb
+++ ruby/caesar_cipher.rb
@@ -2,13 +2,16 @@ def caesar_cipher(phrase, shift)
   shifted_phrase = ''
   phrase.each_char do |char|
     if char.match?(/[a-z]/)
-      shifted_char = (char.ord - 'a'.ord + shift) % 26 + 'a'.ord
-      shifted_phrase << shifted_char.chr
+      # Changes character to ASSCI value, subtracts 'a' ASSCI value to get
+      # position in alaphabet, adds shift, rounds to a if z, and Changes
+      # to shifted character ASSCI value.
+      shifted_char = (char.ord - 'a'.ord + shift) % 26 + 'a'.ord # for lowercase
+      shifted_phrase << shifted_char.chr # Adds character to shifted_phrase
     elsif char.match?(/[A-Z]/)
-      shifted_char = (char.ord - 'A'.ord + shift) % 26 + 'A'.ord
+      shifted_char = (char.ord - 'A'.ord + shift) % 26 + 'A'.ord # for uppercase
       shifted_phrase << shifted_char.chr
     else
-      shifted_phrase << char
+      shifted_phrase << char # for special characters and spaces
     end
   end
   shifted_phrase
