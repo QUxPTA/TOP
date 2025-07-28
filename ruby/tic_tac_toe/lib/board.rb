b643d764c33958e7d2df380128379175a45d740f
QUxPTA <125445356+QUxPTA@users.noreply.github.com>
2025-07-28 23:38:14 +0300

    Add board logic
    - Will handle move validation, check game state, and general grid
    management

--- ruby/tic_tac_toe/lib/board.rb
+++ ruby/tic_tac_toe/lib/board.rb
@@ -15,3 +15,43 @@
 #
 # full?:
 #   true if no empty spaces
+#
+module TicTacToe
+  # Manages the grid, handles move validation, and checks game state
+  class Board
+    def initialize
+      @grid = Array.new(3) { Array.new(3, ' ') }
+    end
+
+    def display
+      puts "\n"
+      @grid.each_with_index do |row, i|
+        puts row.map.with_index { |cell, j| cell.empty? ? ' ' : cell }.join(' | ')
+        puts '--+--+--' unless i == 2
+      end
+      puts "\n"
+    end
+
+    def update(move, symbol)
+      row, col = move
+      @grid[row][col] = symbol
+    end
+
+    def full?
+      @grid.all? { |row| row.none? { |cell| cell == ' ' } }
+    end
+
+    def winner?(symbol)
+      # Rows, columns, diagonals
+      lines = @grid +                                   # rows
+              @grid.transpose +                         # colmuns
+              [[@grid[0][0], @grid[1][1], @grid[2][2]], # diagonal
+               [@grid[0][2], @grid[1][1], @grid[2][0]]] # anti-diagonal
+      lines.any? { |line| line.all? { |cell| cell == symbol } }
+    end
+
+    def valid_move?(row, col)
+      row.between?(0, 2) && col.between?(0, 2) && @grid[row][col] == ' '
+    end
+  end
+end
