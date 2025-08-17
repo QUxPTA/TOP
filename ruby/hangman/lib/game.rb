488e187f14642e61ce5b593b0368cd0b9a4a19a1
QUxPTA <125445356+QUxPTA@users.noreply.github.com>
2025-08-17 10:24:59 +0300

    Add save game functionality
    - Users can now save game at any point by typing save
    - On game load, a user has the option to selection a saved game or
    launch a new one.

--- ruby/hangman/lib/game.rb
+++ ruby/hangman/lib/game.rb
@@ -1,14 +1,23 @@
 require_relative 'game_state'
+require_relative 'game_load'
 require 'colorize'
 
 module Hangman
   class Game
-    def initialize(secret_word = nil)
+    include GameLoad
+
+    def initialize(state_or_secret = nil)
       # initialize game state with random or provided secret_word
-      @state = GameState.new(secret_word)
+      @state = if state_or_secret.is_a?(GameState)
+                 state_or_secret
+               else
+                 GameState.new(state_or_secret)
+               end
     end
 
     def play
+      puts "Type 'save' anytime to save and exit".colorize(:teal)
+
       until @state.won? || @state.lost?
         # Draw Hangman
         draw_hangman(@state.turns_left)
@@ -18,7 +27,13 @@ module Hangman
         puts "Guessed letters: #{(@state.correct_guesses + @state.incorrect_guesses).join(', ')}".colorize(:light_yellow)
 
         print 'Guess a letter: '.colorize(:light_green)
-        guess = gets.chomp
+        guess = gets.chomp.downcase
+
+        if guess == 'save'
+          GameLoad.save_game(@state)
+          puts 'Game saved. Goodbye!'.colorize(:green)
+          return
+        end
 
         # Input Validation: ensures user inputs letters only
         if guess.nil? || guess.empty? || guess.length > 1 || guess.match?(/[^a-zA-Z]/)
--- ruby/hangman/main.rb
+++ ruby/hangman/main.rb
@@ -1,4 +1,17 @@
 require_relative 'lib/game'
+require_relative 'lib/game_load'
+require_relative 'lib/game_state'
 
-game = Hangman::Game.new
-game.play
+puts 'Load saved game? (y/n)'.colorize(:teal)
+choice = gets.chomp.downcase
+
+if choice == 'y'
+  state = Hangman::GameLoad.load_game
+  if state
+    Hangman::Game.new(state).play
+  else
+    puts 'No saved game found!'.colorize(:red)
+  end
+else
+  Hangman::Game.new.play
+end
