2bbc22cc975e6edfb4dd06b70eeb0163af04a902
QUxPTA <125445356+QUxPTA@users.noreply.github.com>
2025-09-02 19:47:15 +0300

    Add #append
    - Adds a new node to the end of the linked list
    - If empty, head points to the new node

--- ruby/linked_list/lib/linked_list.rb
+++ ruby/linked_list/lib/linked_list.rb
@@ -1,16 +1,36 @@
 class LinkedList
-  def initialize(args); end
+  attr_reader :head
+
+  def initialize
+    @head = nil
+  end
 
   def prepend(value)
     new_node = Node.new(value)
     new_node.next_node = head
-    head = new_node
+    @head = new_node # Update head
+  end
+
+  def append(value)
+    new_node = Node.new(value)
+
+    if head.nil?
+      @head = new_node # Where we have an empty list
+    else
+      current = @head
+      current = current.next_node until current.next_node.nil?
+      current.next_node = new_node
+    end
   end
 
   class Node
+    attr_accessor :value, :next_node
+
     def initialize(value)
       @value = value
       @next_node = nil
     end
   end
+
+  private_constant :Node # Keeps Node implementation within LinkedList
 end
